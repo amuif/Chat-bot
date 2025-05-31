593d79020cd2778831e0ea1a163c7381379653c5
amuif <amudihayat@gmail.com>
2025-05-31 18:46:20 +0300

    deployment push

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,6 @@
 
 import LandingComponent from "@/components/landing";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
 const queryClient = new QueryClient();
 
 export default function Home() {
